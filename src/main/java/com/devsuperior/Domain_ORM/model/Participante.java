f2a1dfcd2c2c7daf372f830e6e3df85d9b750f83
nawszera <kkowalskigustavo@gmail.com>
2025-05-27 22:48:59 -0300

    Add entity Participante

---
+++ src/main/java/com/devsuperior/Domain_ORM/model/Participante.java
@@ -0,0 +1,59 @@
+package com.devsuperior.Domain_ORM.model;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table (name = "tb_participantes")
+public class Participante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+
+    public Participante(){}
+
+    public Participante(String email, String nome) {
+        this.email = email;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Participante that = (Participante) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
