e814741d5bed69cbbc5a2aa31275de7b54d2e649
nawszera <kkowalskigustavo@gmail.com>
2025-05-27 22:52:07 -0300

    Add entity Atividade modelado

---
+++ src/main/java/com/devsuperior/Domain_ORM/model/Atividade.java
@@ -0,0 +1,68 @@
+package com.devsuperior.Domain_ORM.model;
+
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.util.Objects;
+@Entity
+@Table(name = "tb_atividade")
+public class Atividade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Double price;
+
+    public Atividade(){}
+
+    public Atividade(String nome, String descricao, Double price) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Atividade atividade = (Atividade) o;
+        return Objects.equals(id, atividade.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
