50e8bf46d943a1ecd3db95e614ce36109bd9e5e5
nawszera <kkowalskigustavo@gmail.com>
2025-05-27 22:40:38 -0300

    Projeto criado e confirando h2DB

---
+++ src/main/java/com/devsuperior/Domain_ORM/DomainOrmApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Domain_ORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/Domain_ORM/DomainOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.Domain_ORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
