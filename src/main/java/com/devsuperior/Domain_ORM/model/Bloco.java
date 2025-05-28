8df35569eeea71fb341915aa3ba63010bea2349d
nawszera <kkowalskigustavo@gmail.com>
2025-05-27 23:10:10 -0300

    Entidade Bloco

---
+++ src/main/java/com/devsuperior/Domain_ORM/model/Bloco.java
@@ -0,0 +1,59 @@
+package com.devsuperior.Domain_ORM.model;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_bloco")
+public class Bloco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant inicio;
+    private Instant fim;
+
+    public Bloco(){}
+
+    public Bloco(Instant inicio, Instant fim) {
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Instant inicio) {
+        this.inicio = inicio;
+    }
+
+    public Instant getFim() {
+        return fim;
+    }
+
+    public void setFim(Instant fim) {
+        this.fim = fim;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Bloco bloco = (Bloco) o;
+        return Objects.equals(id, bloco.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
